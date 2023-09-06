48f38b634e4a5af78e39d43eab75b3ada921c3a4
Sunit-03 <sunitmohan030303@gmail.com>
2023-09-06 09:29:32 +0530

    modification

--- arrays/bin_search.cpp
+++ arrays/bin_search.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int binSearch(int arr[], int size, int key){
     int start=0;
     int end=size-1;
-    int mid=(start+end)/2;
+    int mid=(start+end)/2; //mid=start+(end-start)/2 for large test cases
     while(start<=end){
         if (arr[mid]==key){
             return mid;
